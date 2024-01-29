aa2d7adff0e9eab3bba550d365e8c006865e3f34
Bill Howe <wdhowe@protonmail.com>
2024-01-29 17:55:14 -0600

    Project cleanup. (project.clj and github actions)

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
-(defproject tic_tac_toe "0.1.0-SNAPSHOT"
+(defproject tic_tac_toe "0.1.0"
+  
+  ;;; Project Metadata
   :description "Classic Tic-Tac-Toe Game"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  
+  ;;; Dependencies, Plugins
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot tic-tac-toe.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  
+  ;;; Profiles
+  :profiles {:uberjar {:aot :all}}
+  
+  ;;; Running Project Code
+  :main ^:skip-aot tic-tac-toe.core)
